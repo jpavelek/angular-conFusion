f63064beda781dd10f4702ba1f45071096ac35f3
Jakub Pavelek <jpavelek@live.com>
2017-12-08 12:59:07 +0100

    Add additional fields to Dish and DISHES

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,11 +1,13 @@
 import { Comment } from './comment';
 
 export class Dish {
+    id: number;
     name: string;
     image: string; //URL for image
     category: string;
     label: string;
     price: string;
+    featured: boolean;
     description: string;
     comments: Comment[];
 }
