8825fe74b3d20172b9f9e963a31c6aea96ff11ff
Jakub Pavelek <jpavelek@live.com>
2017-12-08 20:59:24 +0100

    Add cardof featured leader to home page.
    * Intro the leader service to home component.
    * Make method to retrieve featured leader in the service.
    * Add html template to show such card on home page

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -15,4 +15,8 @@ export class LeaderService {
     return LEADERS.filter((lead) => (lead.id === id))[0];
   }
 
+  getFeaturedLeader(): Leader {
+    return LEADERS.filter((lead) => (lead.featured))[0];
+  }
+
 }
