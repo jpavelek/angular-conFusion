dc69d4385c21329e2aa52606108b7b99c5d51820
Jakub Pavelek <jakub.pavelek@linaro.org>
2017-12-16 17:14:46 +0100

    Add initial error messages for form validators.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,6 +12,33 @@ export class ContactComponent implements OnInit {
   feedbackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  }
+
+  validationMessages = {
+    'firstname': {
+      'required': "First name is required",
+      'minlength': "First name must be at least 2 characters long",
+      'maxlenght': "Fist name cannot be more than 25 characters long"
+    },
+    'lastname': {
+      'required': "Last name is required",
+      'minlength': "Last name must be at least 2 characters long",
+      'maxlenght': "Last name cannot be more than 25 characters long"
+    },
+    'telnum': {
+      'required': "Telephone number is required",
+      'pattern': "Telephone number shall contain only numbers"
+    },
+    'email': {
+      'required': "Email is required",
+      'email': "Email not in valid format"
+    }
+  };
 
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -24,12 +51,31 @@ export class ContactComponent implements OnInit {
     this.feedbackForm = this.fb.group({
       firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
       lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
-      telnum: [0, [Validators.required, Validators.pattern ]],
-      email: ['', [Validators.required]],
+      telnum: [0, [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    this.onValueChanged(); // To set/reset form validation messages
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return }
+
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            this.formErrors[field] += messages[key] + " ";
+          }
+        }
+    }
   }
 
   onSubmit() {
