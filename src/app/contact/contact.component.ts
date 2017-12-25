8125d4803bee0c7d090c05335a3a3fbf73bee387
Jakub Pavelek <jpavelek@live.com>
2017-12-25 21:17:30 +0100

    Add expand animation to form and spinner for feedback

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
-import { flyInOut } from '../animations/app.animation';
+import { flyInOut, expand } from '../animations/app.animation';
 import { FeedbackService } from '../services/feedback.service';
 import { Observable } from 'rxjs/Observable';
 
@@ -14,7 +14,8 @@ import { Observable } from 'rxjs/Observable';
     'style': 'display: block;'
   },
   animations: [
-    flyInOut()
+    flyInOut(),
+    expand()
   ]
 })
 export class ContactComponent implements OnInit {
