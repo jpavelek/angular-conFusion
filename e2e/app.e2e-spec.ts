1d4cf8bdb10358bb941e4fcedb0e4a54a5d69adc
Jakub Pavelek <jpavelek@live.com>
2017-12-27 10:44:19 +0100

    Add simple e2e test for page navigation

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,15 @@ describe('con-fusion App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should find "Ristorance Con Fusion" on startup', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
+  });
+
+  it('should navigate to about us page by clicking the link', () => {
+    page.navigateTo('/');
+    let navlink = page.getAllElements('a').get(1); // We should really try andfind the link,not hardcode
+    navlink.click();
+    expect(page.getParagraphText('h3')).toBe('About Us');
   });
 });
